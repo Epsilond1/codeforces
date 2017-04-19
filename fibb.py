44590741f14048b4a783b934080c5df561c6db5a
Poltorak Semyon <epsilond1@yandex.ru>
2017-04-20 00:10:54 +0500

    n-e fibb

--- fibb.py
+++ fibb.py
@@ -13,5 +13,19 @@ def getfibn(n):
     sqrt = math.sqrt(5)
     return round((((1 + sqrt) / 2) ** n - (1 - sqrt) / 2 ** n) / sqrt)
 
+
+def getfibcicle(n):
+    fibb = [0, 1]
+    i = 1
+    while i < n:
+        fibb.append(fibb[0] + fibb[1])
+        i += 1
+        del fibb[0]
+    return fibb[-1]
+
+
+def get_last_digin(n):
+    return n % 10
+
 n = int(input())
-print(getfibball(n))
+print(get_last_digin(getfibcicle(n)))
